a3144e8d884670245d767760a058f77a5a5cfac3
Jack Dewinter <jackdewinter1@gmail.com>
2018-03-31 22:14:07 +0100

    Added datatable component. Also app component constructor setup.

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -3,9 +3,16 @@ import { withTracker } from 'meteor/react-meteor-data';
 import Animal from './Animal.js';
 import { Animals } from '../api/animals.js';
 
+// Import React Table
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+
 // App Component ~ this covers the whole application
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+  }
   getAnimals(){
     return [
       { _id: 1, text: 'Our first dog'},
@@ -26,10 +33,49 @@ export default class App extends Component {
         <header>
           <h1> Find a pet </h1>
         </header>
-        // <ul>
-        //   {this.renderAnimals()}
-        // </ul>
-        
+        <ul>
+          {this.renderAnimals()}
+        </ul>
+        <ReactTable
+        columns={[
+        {
+          Header: "Pet",
+          columns: [
+            {
+              Header: "Pet Name",
+              accessor: "petName"
+            },
+            {
+              Header: "Animal",
+              id: "animal",
+              accessor: d => d.animal
+            }
+          ]
+        },
+        {
+          Header: "Pet Info",
+          columns: [
+            {
+              Header: "Breed",
+              accessor: "breed"
+            },
+            {
+              Header: "Location",
+              accessor: "location"
+            }
+          ]
+        },
+        {
+          Header: 'Stats',
+          columns: [
+            {
+              Header: "Visits",
+              accessor: "visits"
+            }
+          ]
+        }
+      ]}
+        className="-striped -highlight"/>
       </div>
     );
   }

c4b1bd8c5eabb82f400eeab6694cd7a3639efea8
Jack Dewinter <jackdewinter1@gmail.com>
2018-03-31 11:32:16 +0100

    Written some code setting up the app component.

---
+++ imports/ui/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Animal from './Animal.js';
+
+// App Component ~ this covers the whole application
+
+export default class App extends Component {
+  getAnimals(){
+    return [
+      { _id: 1, text 'Our first dog'},
+      { _id: 2, text 'A cat'},
+      { _id: 3, text 'Second dog'},
+    ];
+  }
+
+  renderAnimals(){
+    return this.getAnimals().map((animal) => (
+      <Animal key={animal._id} animal={animal} />
+    ));
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <header>
+          <h1> Find a pet </h1>
+        </header>
+        <ul>
+          {this.renderAnimals()}
+        </ul>
+      </div>
+    );
+  }
+}

f08dd156c27d910948572de7fadae80e365feb56
Jack Dewinter <jackdewinter1@gmail.com>
2018-04-02 19:28:18 +0100

    worked on style

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,3 +1,11 @@
+/**
+* @module App
+* @summary Main component. Rendering front page.
+* @param {Object[]} animals - Animals in the database.
+- **`'STRING'`**: random strings
+- **`'MONGO'`**:  random [`Mongo.ObjectID`](#mongo_object_id) values
+*/
+
 import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import MediaQuery from 'react-responsive';
@@ -20,37 +28,23 @@ export class App extends Component {
     return (
       <div className="container">
         <header>
-          <h1> Find a pet </h1>
+          <center><h1> Find a pet </h1></center>
         </header>
-        <div>
-        <div>Device Test!</div>
-        <MediaQuery query="(min-device-width: 1224px)">
-          <div>You are a desktop or laptop</div>
-          <MediaQuery query="(min-device-width: 1824px)">
-            <div>You also have a huge screen</div>
-          </MediaQuery>
-          <MediaQuery query="(max-width: 1224px)">
-            <div>You are sized like a tablet or mobile phone though</div>
-          </MediaQuery>
-        </MediaQuery>
-        <MediaQuery query="(max-device-width: 1224px)">
-          <div>You are a tablet or mobile phone</div>
-        </MediaQuery>
-        <MediaQuery query="(orientation: portrait)">
-          <div>You are portrait</div>
-        </MediaQuery>
-        <MediaQuery query="(orientation: landscape)">
-          <div>You are landscape</div>
-        </MediaQuery>
-        <MediaQuery query="(min-resolution: 2dppx)">
-          <div>You are retina</div>
-        </MediaQuery>
-      </div>
+
         <PetsTable data={data}/>
       </div>
     );
   }
 }
+
+/**
+* @function Fetches all the data inside the animals collection
+* @summary Retrieving animal details into an object. Updates {@link App.js} when collection is changed.
+* @param {Object[]} animals - Animals in the database.
+- **`'STRING'`**: random strings
+- **`'MONGO'`**:  random [`Mongo.ObjectID`](#mongo_object_id) values
+*
+*/
 
 export default withTracker(() => {
   return {
@@ -61,3 +55,28 @@ export default withTracker(() => {
 // Seperate from the App Component class, we are calling withTracker, giving it our Component.
 // We are giving App the props of our animals
 // App re-renders when the database changes.
+
+// <div>
+// <div>Device Test!</div>
+// <MediaQuery query="(min-device-width: 1224px)">
+//   <div>You are a desktop or laptop</div>
+//   <MediaQuery query="(min-device-width: 1824px)">
+//     <div>You also have a huge screen</div>
+//   </MediaQuery>
+//   <MediaQuery query="(max-width: 1224px)">
+//     <div>You are sized like a tablet or mobile phone though</div>
+//   </MediaQuery>
+// </MediaQuery>
+// <MediaQuery query="(max-device-width: 1224px)">
+//   <div>You are a tablet or mobile phone</div>
+// </MediaQuery>
+// <MediaQuery query="(orientation: portrait)">
+//   <div>You are portrait</div>
+// </MediaQuery>
+// <MediaQuery query="(orientation: landscape)">
+//   <div>You are landscape</div>
+// </MediaQuery>
+// <MediaQuery query="(min-resolution: 2dppx)">
+//   <div>You are retina</div>
+// </MediaQuery>
+// </div>
